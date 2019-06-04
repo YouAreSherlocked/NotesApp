a2081dae30147aa6660e3e961f17746b750f7d18
Mayer Timo <bmayet@bbc-be.local>
2019-06-04 08:27:21 +0200

    init project and note class

---
+++ app/src/main/java/com/example/notesapp/model/Note.java
@@ -0,0 +1,17 @@
+package com.example.notesapp.model;
+
+import java.util.Date;
+
+public class Note {
+    private int id;
+    private String title;
+    private String content;
+    private Boolean isFavourite = false;
+    private Date createdAt;
+
+    public Note() {
+
+    }
+
+
+}
