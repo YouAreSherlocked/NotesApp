9a5568acb4a7295b94c502c4685429bd636dd001
Timo Mayer <mayer.timo@hotmail.com>
2019-06-05 09:34:32 +0200

    Added hash function to login file

--- app/src/main/java/com/example/notesapp/Login.java
+++ app/src/main/java/com/example/notesapp/Login.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Login extends AppCompatActivity {
 
     @Override
@@ -24,5 +27,25 @@ public class Login extends AppCompatActivity {
     public void openRegisterPage() {
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
+    }
+
+    public static String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
